80a34f3d1f6f8da3e809af2eaf32c052bb8b1e89
songqin <980047129@qq.com>
2024-02-19 10:48:20 +0800

    chore:质点生成

---
+++ Unity/Assets/ClothSimulation/Runtime/Scripts/CpuClothSimulation.cs
@@ -0,0 +1,32 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace ClothSimulation.Runtime
+{
+    public class CpuClothSimulation : MonoBehaviour
+    {
+        #region Cloth质点生成
+        
+        public GameObject m_InitGo;
+        public GameObject m_ClothGo;
+
+        [Button("生成预制体")]
+        public void Instantiate()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    var go = GameObject.Instantiate(m_InitGo, new Vector3(i * 2f, j * 2f, 0), Quaternion.identity);
+                    go.transform.SetParent(m_ClothGo.transform);
+                    go.SetActive(true);
+                }
+            }
+        }
+        
+        #endregion
+        
+        
+        
+    }
+}
